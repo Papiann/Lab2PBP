a0ef04132d848bd8b958c53000e7c171e95c5153
Rafi Rasendrya Favian <rafi.rasendrya@ui.ac.id>
2022-09-14 21:41:17 +0700

    Bismillah Kelar Tutorial Lab2PBP

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render
 from wishlist.models import BarangWishlist
 
@@ -11,3 +12,25 @@ def show_wishlist(request):
     }
 
     return render(request, "wishlist.html", context)
+
+def show_xml(request):
+    data = BarangWishlist.objects.all()
+
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = BarangWishlist.objects.all()
+
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_json_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+
+    # Jika JSON
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = BarangWishlist.objects.filter(pk=id)
+
+    # Jika XML
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
